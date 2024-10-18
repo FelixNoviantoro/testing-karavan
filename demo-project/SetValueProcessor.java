22927d077045f5c07c5981ce4ccd0a7a2d94be0c
Felix Noviantoro <59517421+FelixNoviantoro@users.noreply.github.com>
2024-10-18 10:12:54 +0700

    Create SetValueProcessor.java

---
+++ demo-project/SetValueProcessor.java
@@ -0,0 +1,41 @@
+import org.apache.camel.BindToRegistry;
+import org.apache.camel.Configuration;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+import com.google.gson.Gson;
+
+@Configuration
+@BindToRegistry("SetValueProcessor")
+public class SetValueProcessor implements Processor {
+
+    public void process(Exchange exchange) throws Exception {
+        System.out.println("========================= Set Value");
+        Map<String,Map<String,Object>> reqBody = exchange.getIn().getBody(Map.class);
+        Gson gson = new Gson();
+        Map<String, Object> changelogMap = gson.fromJson(gson.toJson(reqBody.get("changelog")), Map.class);
+        System.out.println(gson.toJson(changelogMap.get("items")));
+        List<Map<String, Object>> itemList = (List<Map<String, Object>>) changelogMap.get("items");
+        String fromString = "";
+        String toString = "";
+
+        for (Map<String, Object> item : itemList) {
+            if ("status".equals(item.get("field"))) {
+                // Retrieve fromString and toString values
+                fromString = (String) item.get("fromString");
+                toString = (String) item.get("toString");
+
+                // Print or apply any logic with the values
+                System.out.println("From Status: " + fromString);
+                System.out.println("To Status: " + toString);
+            }
+        }
+
+        boolean statusMatch = "On Process Create VM".equals(fromString) && "Done".equals(toString);
+        System.out.println("BOOLEAN : " + statusMatch);
+        exchange.getIn().setHeader("isComplete", statusMatch);
+    }
+    
+}
